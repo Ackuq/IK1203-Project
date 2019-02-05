7f230891a88be21fc61e7b3897bff27d3f788bc6
Axel Pettersson <axel@pettersson.cc>
2019-02-05 23:55:43 +0100

    Fixed small derps and how the timeout and max input works.

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -7,7 +7,7 @@ public class TCPClient {
     public static String askServer(String hostname, int port, String ToServer) throws  IOException {
 
       // If nothing is going out from client, use other method
-      if(!ToServer) return askServer(hostname, port);
+      if(ToServer == null) return askServer(hostname, port);
 
       // Declare the response from server
       StringBuilder stringInput = new StringBuilder();
@@ -16,8 +16,8 @@ public class TCPClient {
       // Create connection to server
       Socket clientSocket = new Socket(hostname, port);
 
-      // Timout if no response in 5 second
-      clientSocket.setSoTimeout(5000);
+      // Timout if no response in 3 second
+      clientSocket.setSoTimeout(3000);
 
       // Create input and output streams
       DataOutputStream  outputServer = new DataOutputStream(clientSocket.getOutputStream());
@@ -26,19 +26,23 @@ public class TCPClient {
       // Write the output from client to server
       outputServer.writeBytes(ToServer + '\n');
 
+      int counter = 0;
       // Handle input from sercver
       try{
         // WHile there's more input, contiune reading
-        while((response = inputServer.readLine()) != null && response != "\n"){
+        while((response = inputServer.readLine()) != null && response != "\n" && counter < 1024){
           stringInput.append(response + '\n');
+          counter++;
         }
         clientSocket.close();
+        // Return the input of the server response
+        return stringInput.toString();
       }
       catch(IOException e){
         clientSocket.close();
+        // Return the input of the server response
+        return stringInput.toString();
       }
-      // Return the input of the server response
-      return stringInput.toString();
     }
 
     public static String askServer(String hostname, int port) throws  IOException {
@@ -46,14 +50,11 @@ public class TCPClient {
       StringBuilder stringInput = new StringBuilder();
       String response = null;
 
-      // Max 4096 lines of input
-      const int max = 4096;
-
       // Create connection to server
       Socket clientSocket = new Socket(hostname, port);
 
-      // Timeout if no response in 5 seconds
-      clientSocket.setSoTimeout(5000);
+      // Timeout if no response in 3 seconds
+      clientSocket.setSoTimeout(3000);
 
       // Create input stream from server
       BufferedReader inputServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -61,20 +62,19 @@ public class TCPClient {
       // Counter to keep track of how many lines have been recieved
       int counter = 0;
       try{
-        while((response = inputServer.readLine()) != null && response != "\n"){
+        while((response = inputServer.readLine()) != null && response != "\n" && counter < 1024){
           // WHile there's more input, contiune reading
           stringInput.append(response + '\n');
           counter++;
-          if(counter >= max){
-            clientSocket.close();
-          }
         }
         clientSocket.close();
+        // Return the input of the server response
+        return stringInput.toString();
       }
       catch(IOException e){
         clientSocket.close();
+        // Return the input of the server response
+        return stringInput.toString();
       }
-      // Return the input
-      return stringInput.toString();
     }
 }

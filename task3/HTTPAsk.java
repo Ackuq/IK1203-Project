a309c687012e097212e3b17cb9581619ddc394a9
Axel Pettersson <axel@pettersson.cc>
2019-02-07 19:50:13 +0100

    Finished assignment 3.

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -1,20 +1,87 @@
 import java.net.*;
 import java.io.*;
+import tcpclient.TCPClient;
 
 public class HTTPAsk {
     public static void main( String[] args) throws IOException {
-      int port  = Integer.parseInt(args[0]
-      // This is where we put the data from client
-      String fromClient;
+
+      int httpPort;
+      if(args[0]){
+        httpPort = Integer.parseInt(args[0]);
+      } else {
+        httpPort = 8888;
+      }
+
       // Get the port number first argument and open port
-      ServerSocket HTTPSocket = new ServerSocket(port);
+      ServerSocket HTTPSocket = new ServerSocket(httpPort);
+
+      // Our standard HTTP header
+      String HTTPHeader = "HTTP/1.1 200 OK\r\n\r\n";
+
+      // This is where we put the data from client
+      String request;
+      // Data from client
+      String port;
+      String hostname;
+      String string;
 
       while(true){
         try {
-          String servrOutput = TCPClient();
+          // accept client
+          Socket conSocket = HTTPSocket.accept();
+          // Create input and output streams
+          BufferedReader inStream = new BufferedReader(new InputStreamReader(conSocket.getInputStream()));
+          DataOutputStream outStream = new DataOutputStream(new DataOutputStream(conSocket.getOutputStream()));
+
+          // Get the request from client
+          request = inStream.readLine();
+
+          // Split the request into parts
+          String[] params = request.split("[?&= ]");
+
+          // Our response to the server
+          StringBuilder response =  new StringBuilder();
+
+          // Reset the data variables
+          port = null;
+          hostname = null;
+          string = null;
+
+          // Get the values from the string array
+          for(int i = 0; i < params.length; i++){
+            if(params[i].equals("hostname"))
+              hostname = params[++i];
+            else if(params[i].equals("port"))
+              port = params[++i];
+            else if(params[i].equals("string"))
+              string  = params[++i];
+          }
+
+          // First add the HTTP header response
+          response.append(HTTPHeader);
+
+          // If both hostname and port are assignd
+          if(hostname != null && port != null){
+            try{
+              // Add the server response as data
+              response.append(TCPClient.askServer(hostname, Integer.parseInt(port), string));
+            } catch(IOException e) {
+              // If error, we didn't find the server
+              response.append("HTTP/1.1 404 NOT FOUND\r\n");
+            }
+          } else {
+            // If either hostname or port is left empty
+            response.append("HTTP/1.1 400 BAD REQUEST\r\n");
+          }
+
+          // Return our response to client
+          // close connectiion
+          outStream.writeBytes(response.toString());
+          conSocket.close();
         } catch(IOException e) {
           System.err.println(e);
         }
       }
     }
+
 }
--- task3/tcpclient/TCPClient.java
+++ task3/tcpclient/TCPClient.java
@@ -4,77 +4,76 @@ import java.io.*;
 
 public class TCPClient {
 
-    public static String askServer(String hostname, int port, String ToServer) throws  IOException {
+  final static int max = 1024;
 
-      // If nothing is going out from client, use other method
-      if(ToServer == null) return askServer(hostname, port);
+  public static String askServer(String hostname, int port, String ToServer) throws  IOException {
+    // If nothing is going out from client, use other method
+    if(ToServer == null) return askServer(hostname, port);
 
-      // Declare the response from server
-      StringBuilder stringInput = new StringBuilder();
-      String response = null;
+    // Declare the response from server
+    StringBuilder stringInput = new StringBuilder();
+    String response = null;
 
-      // Create connection to server
-      Socket clientSocket = new Socket(hostname, port);
+    // Create connection to server
+    Socket clientSocket = new Socket(hostname, port);
 
-      // Timout if no response in 3 second
-      clientSocket.setSoTimeout(3000);
+    // Timout if no response in 3 second
+    clientSocket.setSoTimeout(3000);
 
-      // Create input and output streams
-      DataOutputStream  outputServer = new DataOutputStream(clientSocket.getOutputStream());
-      BufferedReader inputServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    // Create input and output streams
+    DataOutputStream  outputServer = new DataOutputStream(clientSocket.getOutputStream());
+    BufferedReader inputServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-      // Write the output from client to server
-      outputServer.writeBytes(ToServer + '\n');
+    // Write the output from client to server
+    outputServer.writeBytes(ToServer + '\n');
 
-      int counter = 0;
-      // Handle input from sercver
-      try{
-        // WHile there's more input, contiune reading
-        while((response = inputServer.readLine()) != null && response != "\n" && counter < 1024){
-          stringInput.append(response + '\n');
-          counter++;
-        }
-        clientSocket.close();
-        // Return the input of the server response
-        return stringInput.toString();
+    int counter = 0;
+    // Handle input from sercver
+    try{
+      // WHile there's more input, contiune reading
+      while((response = inputServer.readLine()) != null && counter++ < max){
+        stringInput.append(response + '\n');
       }
-      catch(IOException e){
-        clientSocket.close();
-        // Return the input of the server response
-        return stringInput.toString();
-      }
+      clientSocket.close();
+      // Return the input of the server response
+      return stringInput.toString();
     }
-
-    public static String askServer(String hostname, int port) throws  IOException {
-      // Declare the response from server
-      StringBuilder stringInput = new StringBuilder();
-      String response = null;
-
-      // Create connection to server
-      Socket clientSocket = new Socket(hostname, port);
-
-      // Timeout if no response in 3 seconds
-      clientSocket.setSoTimeout(3000);
-
-      // Create input stream from server
-      BufferedReader inputServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-      // Counter to keep track of how many lines have been recieved
-      int counter = 0;
-      try{
-        while((response = inputServer.readLine()) != null && response != "\n" && counter < 1024){
-          // WHile there's more input, contiune reading
-          stringInput.append(response + '\n');
-          counter++;
-        }
-        clientSocket.close();
-        // Return the input of the server response
-        return stringInput.toString();
-      }
-      catch(IOException e){
-        clientSocket.close();
-        // Return the input of the server response
-        return stringInput.toString();
-      }
+    catch(IOException e){
+      clientSocket.close();
+      // Return the input of the server response
+      return stringInput.toString();
     }
+  }
+
+  public static String askServer(String hostname, int port) throws  IOException {
+    // Declare the response from server
+    StringBuilder stringInput = new StringBuilder();
+    String response = null;
+
+    // Create connection to server
+    Socket clientSocket = new Socket(hostname, port);
+
+    // Timeout if no response in 3 seconds
+    clientSocket.setSoTimeout(3000);
+
+    // Create input stream from server
+    BufferedReader inputServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+    // Counter to keep track of how many lines have been recieved
+    int counter = 0;
+    try{
+      while((response = inputServer.readLine()) != null && counter++ < max){
+        // While there's more input, contiune reading
+        stringInput.append(response + '\n');
+      }
+      clientSocket.close();
+      // Return the input of the server response
+      return stringInput.toString();
+    }
+    catch(IOException e){
+      clientSocket.close();
+      // Return the input of the server response
+      return stringInput.toString();
+    }
+  }
 }
